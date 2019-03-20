bdb78b875f0af9fa49360df110ef0f9f94130ad4
aleccohan <44471274+aleccohan@users.noreply.github.com>
2019-03-19 21:22:15 -0400

    updated contents of .h file

--- BC3/general.h
+++ BC3/general.h
@@ -1,1 +1,67 @@
+/* 
+  Calculator.cpp - A simple arithmetic interpreter.
+  Names: Alec Cohan, Matthew Olker, Issac Shore
+  Class: CSC-4510
+  
+  The program demonstrates two main components of
+  an interpreter/compiler.
+  1) A lexical analyzer to scan the input for
+     LETTERs, DIGITs, or OPERATORs
+     
+     An IDENT consist of a LETTER followed by
+     a LETTER or DIGIT (max 99 characters)
+     AN INT_LIT consists of a sequence of DIGITs
+     (max 99 digits).     
+   
+     An OPERATOR =, +, -, *, /, (, )
+     lexeme[] holds the item read from source
+     nextToken holds the type
+  2) A recursive descent parser
+     The Grammar - EBNF description
+     <expr> → <term> {(+ | -) <term>}
+     <term> → <factor> {(* | /) <factor>}
+     <factor> → id | int_constant | ( <expr>  ) 
+     NOTE: the recusive descent starts at <expr>     
+ */
 
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+using namespace std;
+
+/* Global declarations */
+/* Variables */
+char lexeme [100];  
+char nextChar;     
+int lexLen;
+FILE *in_fp, *fopen();
+
+/* Function declarations */
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+
+void expr();
+void term();
+void factor();
+void error(const char *);
+  
+/* Character classes */
+int charClass;
+#define LETTER 0
+#define DIGIT 1
+#define OPERATOR 99
+
+/* Token codes */
+int token;
+int nextToken;
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
